d789586502ab4a71d73ad35f3a97e204482baa2b
juliethaguti <juliethaguti@gmail.com>
2020-09-03 19:28:50 -0300

    Se añadio contenido al archivo

--- typescript/typescript/clases/destructuracion.ts
+++ typescript/typescript/clases/destructuracion.ts
@@ -1,5 +1,35 @@
 (() =>{
 
+    const avenger = {
+        nombre: 'steve',
+        clave: 'Capitan America',
+        poder: 'Escudo'
+    }
+
+    const extraer = ( { nombre, poder }: any ) => {
+
+        // const { nombre, poder } = avenger;
+
+        console.log( nombre );
+        console.log( poder );
+    }
+
+    const avengers: string[] = ['Thor', 'Ironman', 'Spiderman'];
+
+    const [ , , araña] = avengers;
+
+    //console.log( loki );
+    //console.log( hombre );
+    console.log( araña );
+
+    const extraerArr = ( [ thor, ironman, spiderman] : string[] ) => {
+
+        console.log( thor);
+        console.log( ironman );
+        console.log( spiderman );
+    }
     
+
+    extraerArr( avengers );
 
 })();
