31ed3256e45662cd62b76144fdbd1f4a06c85085
juliethaguti <juliethaguti@gmail.com>
2020-09-03 19:00:40 -0300

    se agrega el contenido de interfaces

--- typescript/typescript/clases/interfaces.ts
+++ typescript/typescript/clases/interfaces.ts
@@ -1,5 +1,26 @@
 (() =>{
 
-    
+    interface Xmen{
+        nombre: string;
+        edad: number;
+    }
+
+    const enviarMision = ( xmen: Xmen ) => {
+
+        console.log( `Enviando a ${ xmen.nombre} a la mision`);
+    }
+
+    const regresarAlCuartel = ( xmen: {nombre:string} ) => {
+
+        console.log( `Enviando a ${ xmen.nombre} a la mision`);
+    }
+
+    const wolverine = {
+        nombre : 'Logan',
+        edad: 60
+    }
+
+    enviarMision( wolverine );
+    regresarAlCuartel( wolverine );
 
 })();
