4645e4ebe12d8593561567a93e1d91a3e7578832
juliethaguti <juliethaguti@gmail.com>
2020-09-04 10:21:57 -0300

    Practica resuelta

--- app.ts/app.ts
+++ app.ts/app.ts
@@ -1,39 +1,56 @@
 (()=>{
 
   // Uso de Let y Const
-  var nombre = 'Ricardo Tapia';
-  var edad = 23;
+  const nombre = 'Ricardo Tapia';
+  const edad = 23;
 
-  var PERSONAJE = {
+  let PERSONAJE = {
     nombre: nombre,
     edad: edad
   };
 
 
   // Cree una interfaz que sirva para validar el siguiente objeto
+
+  interface Superheroe{
+    nombre : string;
+    artesMarciales : string[];
+  }
   var batman = {
     nombre: 'Bruno Díaz',
     artesMarciales: ['Karate','Aikido','Wing Chun','Jiu-Jitsu']
   }
 
   // Convertir esta funcion a una funcion de flecha
-  function resultadoDoble( a, b ){
-    return (a + b) * 2
-  }
+  // function resultadoDoble( a, b ){
+  //   return (a + b) * 2
+  // }
+
+  const resultadoDoble = ( a: number, b: number ) => ( a + b) * 2;
 
   // Función con parametros obligatorios, opcionales y por defecto
   // donde NOMBRE = obligatorio
   //       PODER  = opcional
   //       ARMA   = por defecto = 'arco'
-  function getAvenger( nombre, poder, arma ){
-    var mensaje;
-    if( poder ){
-      mensaje = nombre + ' tiene el poder de: ' + poder + ' y un arma: ' + arma;
-    }else{
-      mensaje = nombre + ' tiene un ' + poder
-    }
-  };
+  // function getAvenger( nombre, poder, arma ){
+  //   var mensaje;
+  //   if( poder ){
+  //     mensaje = nombre + ' tiene el poder de: ' + poder + ' y un arma: ' + arma;
+  //   }else{
+  //     mensaje = nombre + ' tiene un ' + poder
+  //   }
+  // };
 
+  const getAvenger = ( nombre: string, poder?: string, arma: string = 'arco' ) => {
+    
+    var mensaje;
+    
+    if( poder ){
+      mensaje = `${nombre} tiene el poder de: ${poder} y un arma: ${arma}`;
+    }else{
+      mensaje = `${nombre} tiene el poder de: ${poder}`;
+    }
+  }
   // Cree una clase que permita manejar la siguiente estructura
   // La clase se debe de llamar rectangulo,
   // debe de tener dos propiedades:
@@ -42,6 +59,17 @@
   // También un método que calcule el área  =  base * altura,
   // ese método debe de retornar un numero.
 
+  class Rectangulo{
+
+    constructor(public base:number, public altura:number){
+      this.base = base;
+      this.altura = altura;
+    }
+
+    area = ( ) : number => {
+      return this.base * this.altura;
+    };
+  }
 
 
 })();
